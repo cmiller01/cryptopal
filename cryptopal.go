3010ef019a62a2d307d54688e481f5eba218983a
Christian Miller <christian.miller@uber.com>
2019-12-04 00:38:24 -0800

    add problem 3, rework to use pointer receiver for hex

--- cryptopal.go
+++ cryptopal.go
@@ -4,16 +4,23 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"unicode"
+	"unicode/utf8"
 )
 
-// HexToBase64 converts a hex to base64 (bytes) which can be cast as a string
+// HexIn is a struct for taking a hex encoded byte slice as src
+type HexIn struct {
+	Src []byte // a hex encoded byte slice
+}
+
+// ToBase64 converts a hex to base64 (bytes) which can be cast as a string
 // see: https://cryptopals.com/sets/1/challenges/1
-func HexToBase64(src []byte) ([]byte, error) {
+func (h *HexIn) ToBase64() ([]byte, error) {
 	// hex to bytes
-	hexDst := make([]byte, hex.DecodedLen(len(src)))
-	n, err := hex.Decode(hexDst, src)
+	hexDst := make([]byte, hex.DecodedLen(len(h.Src)))
+	n, err := hex.Decode(hexDst, h.Src)
 	if err != nil {
-		return nil, fmt.Errorf("can't decode src %s due to %w", src, err)
+		return nil, fmt.Errorf("can't decode h.Src %s due to %w", h.Src, err)
 	}
 
 	dst := make([]byte, base64.StdEncoding.EncodedLen(n))
@@ -21,11 +28,12 @@ func HexToBase64(src []byte) ([]byte, error) {
 	return dst, nil
 }
 
-// FixedXOR returns a slice of bytes that have been xored against a src
-func FixedXOR(src []byte, xor []byte) ([]byte, error) {
-	srcDecoded := make([]byte, hex.DecodedLen(len(src)))
+// FixedXOR returns a slice of bytes that have been xored against a h.Src
+// see: https://cryptopals.com/sets/1/challenges/2
+func (h *HexIn) FixedXOR(xor []byte) ([]byte, error) {
+	srcDecoded := make([]byte, hex.DecodedLen(len(h.Src)))
 	xorDecoded := make([]byte, hex.DecodedLen(len(xor)))
-	hex.Decode(srcDecoded, src)
+	hex.Decode(srcDecoded, h.Src)
 	hex.Decode(xorDecoded, xor)
 	for i := range srcDecoded {
 		srcDecoded[i] = srcDecoded[i] ^ xorDecoded[i]
@@ -33,4 +41,55 @@ func FixedXOR(src []byte, xor []byte) ([]byte, error) {
 	dest := make([]byte, hex.EncodedLen(len(srcDecoded)))
 	hex.Encode(dest, srcDecoded)
 	return dest, nil
+}
+
+func singleXOR(input []byte, key byte) []byte {
+	res := make([]byte, len(input))
+	for i, value := range input {
+		res[i] = value ^ key
+	}
+	return res
+}
+
+// SingleXOR returns the most likely value that was xored against a string
+func (h *HexIn) SingleXOR() byte {
+	decoded := make([]byte, hex.DecodedLen(len(h.Src)))
+	result := make([]int, 256)
+	hex.Decode(decoded, h.Src)
+	for i := 0; i <= 255; i++ {
+		key := byte(i)
+		res := singleXOR(decoded, key)
+		// "score points" for two letters, points for printable characters
+		// lose points for control characters and invalid encoded utf8 strings
+		var score int
+		for i := range res {
+			// if the whole thing isn't valid utf8 then throw it out...
+			if utf8.Valid(res) {
+				if unicode.IsLetter(rune(res[i])) {
+					score += 2
+				}
+				if unicode.IsPrint(rune(res[i])) {
+					score++
+				}
+				if unicode.IsPunct(rune(res[i])) {
+					score--
+				}
+				if unicode.IsControl(rune(res[i])) {
+					score -= 2
+				}
+			}
+		}
+		result[i] = score
+
+	}
+	var maxScore int
+	var key byte
+	for r := range result {
+		if result[r] >= maxScore {
+			key = byte(r)
+			maxScore = result[r]
+		}
+	}
+	return key
+
 }
--- cryptopal_test.go
+++ cryptopal_test.go
@@ -9,8 +9,9 @@ func TestHexToBase64(t *testing.T) {
 
 	input := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	expected := []byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+	hexIn := &HexIn{Src: input}
 
-	actual, err := HexToBase64(input)
+	actual, err := hexIn.ToBase64()
 	if err != nil {
 		t.Errorf("expected nil error got %#v", err)
 	}
@@ -23,11 +24,23 @@ func TestHexToBase64(t *testing.T) {
 func TestFixedXOR(t *testing.T) {
 	input := []byte("1c0111001f010100061a024b53535009181c")
 	xored := []byte("686974207468652062756c6c277320657965")
+	hexIn := &HexIn{Src: input}
 
 	expected := []byte("746865206b696420646f6e277420706c6179")
 
-	actual, _ := FixedXOR(input, xored)
+	actual, _ := hexIn.FixedXOR(xored)
 	if !bytes.Equal(actual, expected) {
 		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestSingleXOR(t *testing.T) {
+	input := []byte("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	expected := byte(88) // checked manually
+	hexIn := &HexIn{Src: input}
+
+	actual := hexIn.SingleXOR()
+	if actual != expected {
+		t.Errorf("expected magic 88, got %d", actual)
 	}
 }
